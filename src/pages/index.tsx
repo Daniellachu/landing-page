bf0e16c56f58d33a1dcd5716098328da4894bd76
Daniellachu <delacruzdanielleb@gmail.com>
2024-02-26 19:20:19 -0800

    fix responsiveness for header and footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,22 +17,6 @@ export default function Home() {
       className={styles.mainContainer}
     >
       <Navbar/>
-      {/* <div className={styles.headingContainer}>
-        <div className={styles.headerText}>
-            <h1 className={styles.header}>Plan the perfect adventure with Orbit.</h1>
-            <p className={styles.headingDescription}>Your personal AI-powered travel companion! <br></br>Craft personalized itineraries and gain smart budgeting insights — all at your fingertips for effortless adventures.</p>
-            <p className={styles.callToAction}>Download now to experience an unforgettable trip.</p>
-            <div className={styles.buttonContainer}>
-            <Link className={styles.actionButtonPrimary} href={"/"}>
-                <Spline scene="https://prod.spline.design/6ew0QRBXgCW73YCK/scene.splinecode" />
-            </Link>
-            <Link className={styles.actionButtonSecondary} href={"https://orbit-2024.vercel.app/"} target="_blank">
-            <Spline scene="https://prod.spline.design/sYvJtzCfnJwpwazS/scene.splinecode" />
-            </Link>
-            </div>
-        </div>
-        <Spline className={styles.headingAnimation}  scene="https://prod.spline.design/irhheiu-D6AuiGEu/scene.splinecode" />
-      </div> */}
       <Heading/>      
       <Feature/>
       <Pricing/>
